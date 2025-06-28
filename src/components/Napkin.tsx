44f041ff8b33566b6801ca282cc1f8004bec3ff7
sunniekapar <kaparsunnie@gmail.com>
2025-06-28 12:27:33 -0700

    feat: add page load animation

--- src/components/Napkin.tsx
+++ src/components/Napkin.tsx
@@ -1,31 +1,31 @@
-import ArepaGrid from "./ArepaGrid";
-import './Napkin.css'
+import ArepaGrid from './ArepaGrid';
+import './Napkin.css';
 
 const imagesarray = [
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino",
-    "domino"
-]
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+  'domino',
+];
 
-
-function Napkin({ setActiveArepa }: { setActiveArepa: (name: string) => void }) {
-    return (
-        <div>
-        <div className="napkin-banner">Arepas Venezolanas</div>
-        <div className="napkin">
-        <ArepaGrid images = {imagesarray} setActiveArepa={setActiveArepa}/>
-        </div>
-        </div>
-    );
+function Napkin({
+  setActiveArepa,
+}: {
+  setActiveArepa: (name: string) => void;
+}) {
+  return (
+    <div className="napkin">
+      <ArepaGrid images={imagesarray} setActiveArepa={setActiveArepa} />
+    </div>
+  );
 }
 
 export default Napkin;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,37 @@
 import './Home.css';
 import Napkin from '../components/Napkin';
-import manoDer from '../assets/mano_der.png'
-import manoIzq from '../assets/mano_izquierda.png'
 import { useState } from 'react';
 import TransitionScreen from '../components/TransitionScreen';
-
-
 
 function Home() {
   const [activeArepa, setActiveArepa] = useState<string | null>(null);
 
   return (
     <div className="home-container">
-      <div className="home-banner"></div>
-      <img src={manoIzq} className="fixed-hands left" />
-      <img src={manoDer} className="fixed-hands right" />
-      <Napkin setActiveArepa={setActiveArepa} />
-      {activeArepa && <TransitionScreen arepa={activeArepa} onFinish={() => setActiveArepa(null)} />}
+      <div className="left-column"></div>
+
+      <div className="main-content">
+        <div className="banner">
+          <span className="banner-text">Arepas Venezolanas</span>
+        </div>
+        <div className="content-underneath">
+          <Napkin setActiveArepa={setActiveArepa} />
+        </div>
+      </div>
+
+      <div className="right-column"></div>
+
+      {activeArepa && (
+        <TransitionScreen
+          arepa={activeArepa}
+          onFinish={() => setActiveArepa(null)}
+        />
+      )}
     </div>
   );
 }
 
 export default Home;
-
-
-
-
 
 // import './Home.css';
 // import Napkin from '../components/Napkin'
